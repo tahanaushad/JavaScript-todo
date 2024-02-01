22c6185f055538fcc3e6b99c678c8bfde8484403
tahanashad <tahanaushad6@gmail.com>
2024-02-01 20:44:48 +0500

    Done mark done

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
-let todos =  []
+let todos = []
 
 function submitTodo() {
     myInputEl = document.querySelector(".myInput");
+    if(myInputEl.value == "") return
     todos.push(myInputEl.value)
     myInputEl.value = ""
 
     renderTodos()
 }
 
-function renderTodos (){
-    const todoContainerEl = document.getElementById("todoContainer") 
+function renderTodos() {
+    const todoContainerEl = document.getElementById("todoContainer")
     todoContainerEl.innerHTML = ""
 
-    for(let i = 0; i < todos.length; i++){
+    for (let i = 0; i < todos.length; i++) {
         const liEl = document.createElement("li");
         liEl.innerText = todos[i];
+        liEl.id = i;
         const deleteBtnEl = document.createElement("button")
         deleteBtnEl.innerText = "Delete"
-        deleteBtnEl.addEventListener("click", function (){
+        deleteBtnEl.addEventListener("click", function () {
             removeTodos(i)
         })
 
-        liEl
+        const doneBtnEl = document.createElement("button")
+        doneBtnEl.innerText = "Done"
+        doneBtnEl.addEventListener("click", function () {
+            doneTodos(i)
+        })
+
         todoContainerEl.appendChild(liEl);
         todoContainerEl.appendChild(deleteBtnEl);
+        todoContainerEl.appendChild(doneBtnEl);
     }
 }
 
-function removeTodos (index){
-todos.splice(index, 1)
-renderTodos()
+function removeTodos(index) {
+    todos.splice(index, 1)
+    renderTodos()
+}
+
+function doneTodos(index) {
+document.getElementById(index).style.textDecoration = "line-through"
+    
+    // renderTodos()
 }
 
 
